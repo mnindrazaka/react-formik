2a9def3d19c701ce7a73ce1d4892611927cc3b3c
mnindrazaka <mnindrazaka@gmail.com>
2019-03-29 07:01:09 +0700

    initial form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+import { Formik, Form, FormikErrors } from 'formik'
+
+interface IFormValues {
+  email: string
+  password: string
+}
 
 class App extends Component {
   render() {
@@ -8,21 +15,83 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            validate={values => {
+              const errors: FormikErrors<IFormValues> = {}
+
+              if (!values.email) errors.email = 'Required'
+              if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+              ) {
+                errors.email = 'You must supply a valid email address'
+              }
+
+              if (values.password.length < 8) {
+                errors.password = 'Passwords must be at least 8 characters'
+              }
+
+              if (values.email === values.password) {
+                errors.password =
+                  "Your password shouldn't be the same as your email"
+              }
+
+              return errors
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => setSubmitting(false), 3 * 1000)
+            }}
           >
-            Learn React
-          </a>
+            {props => (
+              <Form>
+                <label htmlFor="email">Email</label>
+                <div>
+                  <input
+                    name="email"
+                    type="email"
+                    placeholder="Enter your account email"
+                    value={props.values.email}
+                    onChange={props.handleChange}
+                    onBlur={props.handleBlur}
+                  />
+                  {props.errors.email && props.touched.email && (
+                    <div style={{ color: 'red' }}>{props.errors.email}</div>
+                  )}
+                </div>
+                <label htmlFor="password">Password</label>
+                <div>
+                  <input
+                    name="password"
+                    type="password"
+                    placeholder="Enter your account password"
+                    value={props.values.password}
+                    onChange={props.handleChange}
+                    onBlur={props.handleBlur}
+                  />
+                  {props.errors.password && props.touched.password && (
+                    <div style={{ color: 'red' }}>{props.errors.password}</div>
+                  )}
+                </div>
+                <input
+                  type="submit"
+                  value="Submit"
+                  disabled={props.isSubmitting}
+                />
+                &nbsp;
+                <input
+                  type="reset"
+                  value="Reset"
+                  onClick={props.handleReset}
+                  disabled={!props.dirty || props.isSubmitting}
+                />
+              </Form>
+            )}
+          </Formik>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
